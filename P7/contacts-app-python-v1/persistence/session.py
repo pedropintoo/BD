fa346d403d5aff42eeb6d3fc2fc1500d975d9bdd
Pedro Pinto <pmap@ua.pt>
2024-04-18 15:26:11 +0100

    poetry fix + install

--- P7/contacts-app-python-v1/persistence/session.py
+++ P7/contacts-app-python-v1/persistence/session.py
@@ -25,3 +25,6 @@ def conn_string() -> str:
 def create_connection():
     my_conn_string = conn_string()
     return pyodbc.connect(my_conn_string)
+
+# if __name__ == "__main__":
+#     print(conn_string())
