da3fa5ee676becc72fe28f90188480fd0b7e536d
Pedro Pinto <pmap@ua.pt>
2024-05-09 09:59:03 +0100

    p8

---
+++ P8/sql_8_2.sql
@@ -0,0 +1,12 @@
+GO
+USE P8;
+GO
+
+CREATE TABLE mytemp (
+    rid BIGINT /*IDENTITY (1, 1)*/ NOT NULL,
+    at1 INT NULL,
+    at2 INT NULL,
+    at3 INT NULL,
+    lixo varchar(100) NULL,
+    PRIMARY KEY (rid)
+);
